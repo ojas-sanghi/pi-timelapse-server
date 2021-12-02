0ec153c4cfc0e2f956f9b7d263f47636afbe2457
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-12-02 08:40:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,18 @@ def hello():
 # http://127.0.0.1:5000/update-status?status=Python
 @app.route('/update-status')
 def update_status():
+    global status
     status = request.args['status']
+    print("GOT! STATUS!")
 
 @app.route('/get-status')
 def get_status():
     return '''<h1>Current status: {}</h1>'''.format(status)
+
+    old_status = status
+    while True:
+        if status != old_status:
+            get_status()
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
