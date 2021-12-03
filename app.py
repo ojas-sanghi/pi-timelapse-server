da7840f2980c9fad97b55da8ea3c990232d81cc6
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-12-03 08:19:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import date, datetime
+import pytz
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -16,21 +17,29 @@ def update_status():
     global status, time_received
     
     status = request.args['status']
-    time_received = datetime.now()
+
+    mst = pytz.timezone("America/Phoenix")
+    time_received = datetime.now(mst)
     
     print("GOT! STATUS!")
     return ""
 
 @app.route('/get-status')
 def get_status():
+    timediff = datetime.now() - time_received
+    timediff_s = timediff.total_seconds()
+    minutes_diff  = divmod(timediff_s, 60)[0]
+
     return '''
     <h1>
         Current status: {}
         <br>
         Time received: {}
+        <br>
+        Time since last: {}
     </h1>
 
-    '''.format(status, time_received.strftime("%H:%M:%S %d %B %Y"))
+    '''.format(status, time_received.strftime("%H:%M:%S %d %B %Y"), minutes_diff)
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
